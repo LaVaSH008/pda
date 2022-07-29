ff75046d35659c70b4071281ef50dda4724be637
LaVaSHoCK <danrumu@gmail.com>
2022-07-29 14:57:42 +0300

    First init commit. Testing vue framework

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,75 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="app">
+        <form @submit.prevent>
+            <h4>Create post</h4>
+            <input @value="title" @input="title = $event.target.value" class="input" type="text" placeholder="Title">
+            <input @value="body" @input="body = $event.target.value" class="input" type="text" placeholder="Description">
+            <button class="btn" @click="createPost">Create</button>
+        </form>
+        <div class="post" v-for="post in posts">
+            <div><strong>Название: </strong>{{ post.title }}</div>
+            <div><strong>Описание: </strong>{{ post.body }}</div>
+        </div>
+    </div>
+    
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+export default{
+    data() { 
+        return{
+            posts: [
+                {id: 1, title: 'JavaScript1', body: 'Description'},
+            ],
+            title: '',
+            body: '',
+        }
+    },
+    methods: {
+        createPost(){
+            const newPost = {
+                id: Date.now(),
+                title: this.title,
+                body: this.body
+            }
+            this.posts.push(newPost);
+            this.title = '';
+            this.body = '';
+        }
+    }
 }
 </script>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style>
+    * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
+    .app {
+        margin: 20px;
+    }
+    form {
+        display: flex;
+        flex-direction: column;
+    }
+    .btn {
+        margin-top: 15px;
+        align-self: flex-end;
+        padding: 10px 15px;
+        background: none;
+        color: teal;
+        border: 1px solid teal;
+    }
+    .post {
+        padding: 15px;
+        border: 2px solid teal;
+        margin-top: 15px;
+    }
+    .input {
+        width: 100%;
+        border: 1px solid teal;
+        padding: 10px 15px;
+        margin-top: 15px;
+    }
 </style>
